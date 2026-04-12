eadfa03f08f7e904bc6d068cd7f6848b921e802d
danielsouzzz <danielsouzanovo@gmail.com>
2026-04-12 20:27:57 -0300

    feat: adicionar entidade country

--- src/main/java/com/project/viacep/model/Country.java
+++ src/main/java/com/project/viacep/model/Country.java
@@ -1,4 +1,28 @@
 package com.project.viacep.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String capital;
+
+    @Column
+    private String flagUrl;
 }
